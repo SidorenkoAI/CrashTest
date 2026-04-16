b641fba65694f8f8a9a029699ddafbc765eba074
sai <sai@mail.ru>
2026-04-16 20:21:28 +0700

    test stick

--- ws2812/ws2812.ino
+++ ws2812/ws2812.ino
@@ -1,4 +1,5 @@
 #include <Adafruit_NeoPixel.h>
+#include "Stick.h"
 
 #define MATRIX_PIN 2
 #define ROWS 20
@@ -6,15 +7,17 @@
 #define NUM_PIXELS (ROWS * COLS)
 
 Adafruit_NeoPixel matrix(NUM_PIXELS, MATRIX_PIN, NEO_GRB + NEO_KHZ800);
-
+Stick st;
 void setup() {
   matrix.begin();
   for (int i = 0; i < NUM_PIXELS; i++) {
     matrix.setPixelColor(i, matrix.Color(0, 0, 150)); // Blue
   }
   matrix.show();
+  Serial.begin(9600);
 }
 
 void loop() {
+  Serial.println(st.getValue());
   delay(10);
 }
