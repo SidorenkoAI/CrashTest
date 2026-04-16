5c579c1cddab5e0b410a41003b399044c150a81f
sai <sai@mail.ru>
2026-04-16 20:26:40 +0700

    Добавлена точка

--- ws2812/ws2812.ino
+++ ws2812/ws2812.ino
@@ -10,14 +10,19 @@ Adafruit_NeoPixel matrix(NUM_PIXELS, MATRIX_PIN, NEO_GRB + NEO_KHZ800);
 Stick st;
 void setup() {
   matrix.begin();
-  for (int i = 0; i < NUM_PIXELS; i++) {
-    matrix.setPixelColor(i, matrix.Color(0, 0, 150)); // Blue
-  }
-  matrix.show();
+  
   Serial.begin(9600);
 }
 
 void loop() {
   Serial.println(st.getValue());
+  int dot = 150;
+  for (int i = 0; i < NUM_PIXELS; i++) {
+    if (i == dot)
+      matrix.setPixelColor(i, matrix.Color(255, 127, 0)); 
+    else
+      matrix.setPixelColor(i, matrix.Color(0, 0, 150)); 
+  }
+  matrix.show();
   delay(10);
 }
