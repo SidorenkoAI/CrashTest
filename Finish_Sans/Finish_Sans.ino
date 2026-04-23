f93113e37606d4e269df58b56730bc2806af2f3c
Sans <anton.rub10@mail.ru>
2026-04-23 17:12:53 +0300

    Помощь

--- Finish_Sans/Finish_Sans.ino
+++ Finish_Sans/Finish_Sans.ino
@@ -1,3 +1,5 @@
+#include <GTimer.h>
+
 #define BUTTON_PIN 2
 #define PIR_PIN 3
 #define SVET_PIN 4
@@ -8,18 +10,35 @@ const float RL10 = 50;
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(BUTTON_PIN, INPUT);
   pinMode(PIR_PIN, INPUT);
   pinMode(SVET_PIN, OUTPUT);
 }
 
 void loop() {
+  static uint32_t tmr;
+
   int analogValue = analogRead(PHOTO_APIN);
   float voltage = analogValue / 1024. * 5;
   float resistance = 2000 * voltage / (1 - voltage / 5);
   float lux = pow(RL10 * 1e3 * pow(10, GAMMA) / resistance, (1 / GAMMA));
 
-  Serial.println(lux);
+  int PIR = digitalRead(PIR_PIN);
+  int BUTTON = digitalRead(BUTTON_PIN);
 
+  // Первая задача
+  if (PIR == 1){
+    digitalWrite(SVET_PIN, HIGH);
+    EVERY_S(10) {
+      digitalWrite(SVET_PIN, LOW);
+    }
+  }
+
+  if (BUTTON == 1){
+    digitalWrite(SVET_PIN, HIGH);
+    EVERY_MS(30000) {
+      digitalWrite(SVET_PIN, LOW);
+    }
+  }
 }
