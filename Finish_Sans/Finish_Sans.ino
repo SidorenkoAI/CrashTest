fdf67f484b160c6d8507e9e60ff091edc475e91d
sai <sai@mail.ru>
2026-04-23 21:14:03 +0700

    modify ino

--- Finish_Sans/Finish_Sans.ino
+++ Finish_Sans/Finish_Sans.ino
@@ -3,9 +3,6 @@
 #define SVET_PIN 4
 #define BUZZER_PIN 5
 #define PHOTO_APIN A0
-const float GAMMA = 0.7;
-const float RL10 = 50;
-
 
 void setup() {
   Serial.begin(9600);
@@ -15,11 +12,12 @@ void setup() {
 }
 
 void loop() {
-  int analogValue = analogRead(PHOTO_APIN);
-  float voltage = analogValue / 1024. * 5;
-  float resistance = 2000 * voltage / (1 - voltage / 5);
-  float lux = pow(RL10 * 1e3 * pow(10, GAMMA) / resistance, (1 / GAMMA));
-
-  Serial.println(lux);
+  if (digitalRead(PIR_PIN)){
+    digitalWrite(SVET_PIN, HIGH);
+  }
+  else
+    digitalWrite(SVET_PIN, LOW);
+  
+  
 
 }
