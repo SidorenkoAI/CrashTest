29e6bacd1b1c1a423a7effa432eb5531cffb6861
SJI381 <psasa0474@gmail.com>
2026-04-26 14:08:33 +0300

    Signed-off-by: SJI381 <psasa0474@gmail.com>

--- Lampa_Final/Lampa_Final.ino
+++ Lampa_Final/Lampa_Final.ino
@@ -1,15 +1,46 @@
 #include <RGBrejim.h>
-int movement=3;
-int fotores=5;
-RGBrejim RGB(4,10,7,8);
+int movement=8;
+int fotores=6;
+RGBrejim RGB(7,9,10,11);
+int pot=A0;
 unsigned long Timer = 0;
 bool On=false;
 bool Speedmovement=false;
 bool Actimer=false;
+int Rpot;
+int Gpot;
+int Bpot;
 void setup() {
  Serial.begin(9600);
 }
-
+void LightPot(){
+  int pokazpot=analogRead(pot);
+int  LightSvet=map(pokazpot,0,1023,0,255);
+  switch(RGB.getLong()){
+ //   case 0:{
+ //   Rpot=10;
+  //  Gpot=10;
+  //  Bpot=10;
+   // break;
+   // }
+    case 1:{
+      Rpot=LightSvet;
+      break;
+    }
+    case 2:{
+      Gpot=LightSvet;
+      break;
+    }
+    case 3:{
+      Bpot=LightSvet;
+      break;
+    }
+    case 4:{
+      RGB.ResetLong();
+      break;
+    }
+  }
+}
 void loop() {
 bool  fotoresPokas=digitalRead(fotores);
   //Serial.print(fotoresPokas);
@@ -25,17 +56,18 @@ Speedmovement=digitalRead(movement);
     Timer=millis();
   }
   if(On==true){
- RGB.RGBsvet();
+ LightPot();
+ RGB.RGBsvet(Rpot,Gpot,Bpot);
   } else {
     RGB.RGBoff();
   }
   if(Actimer){
   if(millis()-Timer>=30000){
-     Serial.println("30 секунд истекло!");
      On=false;
      Actimer=false;
      } else {
       On=true; 
      }
   } 
+  Serial.print(analogRead(pot));
 }
